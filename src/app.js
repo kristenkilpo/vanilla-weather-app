c1971c11fd70ba53cb789efbad0ae3add05ca63a
kristenkilpo <127740305+kristenkilpo@users.noreply.github.com>
2023-04-27 11:40:19 -0500

    Added API

---
+++ src/app.js
@@ -0,0 +1,16 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.weather[0].description;
+}
+
+let apiKey = "7fcabeda1fa0b32951e60534dd81c934";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
